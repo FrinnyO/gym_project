9d98a32e57f21ab9a2cf6a5b1fb5dffeaf3e57a2
FrinnyO <Toxa.Vol@yandex.ru>
2026-04-06 21:28:12 +0300

    little change 2

--- gym_project/gym_project/settings.py
+++ gym_project/gym_project/settings.py
@@ -65,13 +65,8 @@ WSGI_APPLICATION = 'gym_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gym_bd',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'OPTIONS':{ 'client_encoding': 'UTF8',},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
